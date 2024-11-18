bdee666bbcce4eb0fa1488bb598605ec87b4a28a
Luckxx24 <127165929+Luckxx24@users.noreply.github.com>
2024-11-18 22:28:01 +0700

    main

---
+++ src/utils/firebase.ts
@@ -0,0 +1,20 @@
+// src/utils/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8am4nQJZG7BzAnSHDD0wmo2ZpfpaRTlI",
+    authDomain: "vue-firebase-81a2d.firebaseapp.com",
+    projectId: "vue-firebase-81a2d",
+    storageBucket: "vue-firebase-81a2d.firebasestorage.app",
+    messagingSenderId: "127125380906",
+    appId: "1:127125380906:web:bfcf19e110896b0beac853",
+    measurementId: "G-22KXC7CKH4"
+  };
+  
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
